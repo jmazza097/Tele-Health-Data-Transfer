8d5800597dc15b826e7bfb8e80a8cdabc1ed8d80
Jack Mazza <jmazza097@gmail.com>
2020-09-28 16:29:07 -0500

    Initial Commit

---
+++ ForReal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForReal
+//
+//  Created by Glen Evans on 5/6/20.
+//  Copyright © 2020 Jack Mazza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

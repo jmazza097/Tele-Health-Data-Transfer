559204a662252cd1d8e4296731fad75a2ad07d0e
Jack Mazza <jmazza097@gmail.com>
2020-10-11 19:37:14 -0500

    Seperate view controllers for login and steps

---
+++ ForReal/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  ForReal
+//
+//  Created by Glen Evans on 10/7/20.
+//  Copyright © 2020 Jack Mazza. All rights reserved.
+//
+
+import SwiftUI
+import FirebaseUI
+import UIKit
+
+
+class LoginViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func loginTap(_ sender: UIButton) {
+        //Get Default auth UI
+            let authUI = FUIAuth.defaultAuthUI()
+
+            // Check that it isn't nil
+            guard authUI != nil else {
+                return
+            }
+
+            //set Delegate
+        authUI?.delegate = (self as FUIAuthDelegate)
+            authUI?.providers = [FUIEmailAuth()]
+
+            //Get reference to auth UI
+            let authViewController = authUI!.authViewController()
+            present(authViewController, animated: true, completion: nil)
+        }
+    
+    
+}
+extension LoginViewController: FUIAuthDelegate {
+
+func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+
+    // Check for error
+    guard error == nil else {
+        return
+        }
+    performSegue(withIdentifier: "goHome", sender: self)
+
+    }
+}

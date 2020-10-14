a82bfe5492a8038d65f3ece52093b721232f1ce9
Jack Mazza <jmazza097@gmail.com>
2020-10-14 11:53:08 -0500

    email and login fully working

---
+++ ForReal/SAButton.swift
@@ -0,0 +1,30 @@
+//
+//  SAButton.swift
+//  ForReal
+//
+//  Created by Glen Evans on 10/12/20.
+//  Copyright © 2020 Jack Mazza. All rights reserved.
+//
+import UIKit
+
+class SAButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    
+    private func setupButton() {
+        setTitleColor(.white, for: .normal)
+        backgroundColor     = UIColor(red: 255/255, green: 5/255, blue: 41/255, alpha: 1.0)
+        titleLabel?.font    = .boldSystemFont(ofSize: 20)
+        layer.cornerRadius  = frame.size.height / 2
+    }
+}
--- Podfile
+++ Podfile
@@ -5,9 +5,9 @@ target 'ForReal' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 	
-  pod 'Firebase/Firestore'
   pod 'Firebase/Auth'
-  pod 'Firebase/Analytics'
+  pod 'Firebase/Firestore'
   pod 'FirebaseUI'
 
 end
+
